d1a13323a6a3eaefa52f578fc79551754952e583
codewithshadyy <kipkoechshadrack10@gmail.com>
2025-11-28 16:12:20 +0000

    learning progamming paradiggms

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,4 +1,3 @@
-
 import unittest
 from simple_calculator import SimpleCalculator
 
@@ -8,42 +7,43 @@ class TestSimpleCalculator(unittest.TestCase):
         """Create a SimpleCalculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    
-    def test_add(self):
-        
+  
+    def test_addition(self):
+        """Test the addition method."""
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(-5, -5), -10)
         self.assertEqual(self.calc.add(2.5, 2.5), 5.0)
 
-   
-    def test_subtract(self):
-       
+    def test_subtraction(self):
+        """Test the subtraction method."""
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(3, 5), -2)
         self.assertEqual(self.calc.subtract(-3, -3), 0)
         self.assertEqual(self.calc.subtract(5.5, 2.5), 3.0)
 
-   
+    
     def test_multiply(self):
-       
+        """Test the multiplication method."""
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-2, 3), -6)
         self.assertEqual(self.calc.multiply(-2, -3), 6)
         self.assertEqual(self.calc.multiply(2.5, 2), 5.0)
         self.assertEqual(self.calc.multiply(0, 10), 0)
 
-   
+  
     def test_divide(self):
-        
+        """Test the division method."""
         self.assertEqual(self.calc.divide(6, 3), 2)
         self.assertAlmostEqual(self.calc.divide(7, 2), 3.5)
         self.assertEqual(self.calc.divide(-6, 3), -2)
+
+       
         self.assertEqual(self.calc.divide(5.5, 2.2), 2.5)
 
-    def test_divide_by_zero(self):
+      
         self.assertIsNone(self.calc.divide(10, 0))
-        self.assertIsNone(self.calc.divide(0, 0))  
+        self.assertIsNone(self.calc.divide(0, 0))
 
 if __name__ == "__main__":
     unittest.main()
